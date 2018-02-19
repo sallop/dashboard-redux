7d568d31c68cbdf470b95c1d0f5a9f0b4c8ece55
sallop <shiogai1987@yahoo.co.jp>
2018-02-19 16:50:00 +0900

    add: fontawesome icon samples

---
+++ src/components/atoms/FaSamples.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import '@fortawesome/fontawesome-free-solid';
+// import '@fortawesome/fontawesome-free-brands';
+
+const FaSamples = () => (
+  <div>
+    <ul>
+      <li>
+        <FontAwesomeIcon icon={['fas', 'power-off']} />
+        <FontAwesomeIcon icon={['fas', 'cog']} />
+        <FontAwesomeIcon icon={['fas', 'cogs']} />
+        <FontAwesomeIcon icon={['fas', 'chevron-down']} />
+        <FontAwesomeIcon icon={['fas', 'chevron-left']} />
+        <FontAwesomeIcon icon={['fas', 'chevron-right']} />
+        <FontAwesomeIcon icon={['fas', 'chevron-up']} />
+        <FontAwesomeIcon icon={['fas', 'copy']} />
+        <FontAwesomeIcon icon={['fas', 'redo']} />
+        <FontAwesomeIcon icon={['fas', 'redo-alt']} />
+        <FontAwesomeIcon icon={['fas', 'trash']} />
+        <FontAwesomeIcon icon={['fas', 'trash-alt']} />
+        <FontAwesomeIcon icon={['fas', 'sync']} />
+        <FontAwesomeIcon icon={['fas', 'sync-alt']} />
+        <FontAwesomeIcon icon={['fas', 'link']} />
+        <FontAwesomeIcon icon={['fas', 'unlink']} />
+      </li>
+      <li>
+        <FontAwesomeIcon icon={['fas', 'sign-in-alt']} />
+        <FontAwesomeIcon icon={['fas', 'sign-out-alt']} />
+        <FontAwesomeIcon icon={['fas', 'sort']} />
+        <FontAwesomeIcon icon={['fas', 'sort-alpha-down']} />
+        <FontAwesomeIcon icon={['fas', 'sort-alpha-up']} />
+        <FontAwesomeIcon icon={['fas', 'sort-amount-down']} />
+        <FontAwesomeIcon icon={['fas', 'sort-amount-up']} />
+        <FontAwesomeIcon icon={['fas', 'sort-numeric-down']} />
+        <FontAwesomeIcon icon={['fas', 'sort-numeric-up']} />
+        <FontAwesomeIcon icon={['fas', 'list']} />
+        <FontAwesomeIcon icon={['fas', 'list-alt']} />
+        <FontAwesomeIcon icon={['fas', 'list-ol']} />
+        <FontAwesomeIcon icon={['fas', 'list-ul']} />
+        <FontAwesomeIcon icon={['fas', 'lock']} />
+        <FontAwesomeIcon icon={['fas', 'lock-open']} />
+        <FontAwesomeIcon icon={['fas', 'save']} />
+      </li>
+      <li>
+        <FontAwesomeIcon icon={['fas', 'user']} />
+        <FontAwesomeIcon icon={['fas', 'user-md']} />
+        <FontAwesomeIcon icon={['fas', 'user-plus']} />
+        <FontAwesomeIcon icon={['fas', 'user-secret']} />
+        <FontAwesomeIcon icon={['fas', 'user-times']} />
+        <FontAwesomeIcon icon={['fas', 'users']} />
+        <FontAwesomeIcon icon={['fas', 'universal-access']} />
+        <FontAwesomeIcon icon={['fas', 'male']} />
+        <FontAwesomeIcon icon={['fas', 'female']} />
+        <FontAwesomeIcon icon={['fas', 'child']} />
+        <FontAwesomeIcon icon={['fas', 'blind']} />
+        <FontAwesomeIcon icon={['fas', 'deaf']} />
+        <FontAwesomeIcon icon={['fas', 'wheelchair']} />
+        <FontAwesomeIcon icon={['fas', 'sign-language']} />
+      </li>
+      <li>
+        <FontAwesomeIcon icon={['fas', 'coffee']} />
+        <FontAwesomeIcon icon={['fas', 'leaf']} />
+        <FontAwesomeIcon icon={['fas', 'lemon']} />
+        <FontAwesomeIcon icon={['fas', 'gem']} />
+        <FontAwesomeIcon icon={['fas', 'moon']} />
+      </li>
+      <li>
+        <FontAwesomeIcon icon={['fas', 'chess']} />
+        <FontAwesomeIcon icon={['fas', 'chess-board']} />
+        <FontAwesomeIcon icon={['fas', 'chess-king']} />
+        <FontAwesomeIcon icon={['fas', 'chess-queen']} />
+        <FontAwesomeIcon icon={['fas', 'chess-bishop']} />
+        <FontAwesomeIcon icon={['fas', 'chess-rook']} />
+        <FontAwesomeIcon icon={['fas', 'chess-knight']} />
+        <FontAwesomeIcon icon={['fas', 'chess-pawn']} />
+      </li>
+    </ul>
+  </div>
+);
+
+export default FaSamples;
