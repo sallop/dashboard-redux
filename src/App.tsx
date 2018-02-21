cf1692a531f304c5dcbb415529ee50f5d81b89b0
sallop <shiogai1987@yahoo.co.jp>
2018-02-21 20:38:23 +0900

    modify: delete garbage line

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,15 @@
 import * as React from 'react';
-import GlobalNavbar from './containers/organisms/GlobalNavbar';
-import {
-  Nav,
-  NavItem,
-  NavLink,
-} from 'reactstrap';
 
 import './App.css';
 import Router from './Router';
 import '@fortawesome/fontawesome-free-solid';
 import '@fortawesome/fontawesome-free-brands';
-// import FaSamples from './components/atoms/FaSamples';
 
 class App extends React.Component {
   render() {
-    const links: string[] = [
-      'home',
-      'signin',
-      'signup',
-    ];
     return (
       <div>
-        <GlobalNavbar />
         <Router/>
-        <p>List Based</p>
-        <Nav vertical={true}>
-          {
-            links.map((link, idx) => (
-              <NavItem key={idx}>
-                <NavLink key={'link' + idx} href={link}>{link}</NavLink>
-              </NavItem>
-            ))
-          }
-        </Nav>
-        {/* <FaSamples/> */}
       </div>
     );
   }
