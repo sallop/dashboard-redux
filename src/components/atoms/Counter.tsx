77591d9b22892706064961deabae33466ff33b50
sallop <shiogai1987@yahoo.co.jp>
2018-03-07 16:45:00 +0900

    fix: remove comment out

--- src/components/atoms/Counter.tsx
+++ src/components/atoms/Counter.tsx
@@ -1,54 +1,54 @@
 import * as React from 'react';
-// import { pushIncrement, pushDecrement } from '../../actions';
-// 
-// const styles = {
-//   counter: {
-//     width: '100px',
-//     fontSize: '0.75rem',
-//     border: '1px solid black',
-//     backgroundColor: '#9db6dd',
-//   },
-//   counterDisplay: {
-//     display: 'inline',
-//     fontSize: '2rem',
-//     color: '#586272'
-//   },
-//   button: {
-//     display: 'inline-block',
-//     color: '#8f9eb7',
-//     backgroundColor: '#586272',
-//   }
-// };
-// 
-// export interface StateProps {
-//   value: number;
-// }
-// 
-// export interface DispatchProps {
-//   onIncrement: typeof pushIncrement;
-//   onDecrement: typeof pushDecrement;
-// }
-// 
-// interface Props extends StateProps, DispatchProps {}
-// 
-// interface State {}
-// 
-// class Counter extends React.Component<Props, State> {
-//   constructor(props: Props, state: State) {
-//     super(props);
-//   }
-// 
-//   render() {
-//     const { value, onIncrement, onDecrement } = this.props;
-//     return (
-//       <div className="counter" style={styles.counter}>
-//         <div style={styles.counterDisplay}>{value}</div>
-//         <button onClick={onIncrement} style={styles.button}>+</button>
-//         <button onClick={onDecrement} style={styles.button}>-</button>
-//       </div>
-//     );
-//   }
-// }
+import { pushIncrement, pushDecrement } from '../../actions';
 
-const Counter = () => <div>Counter</div>;
+const styles = {
+  counter: {
+    width: '100px',
+    fontSize: '0.75rem',
+    border: '1px solid black',
+    backgroundColor: '#9db6dd',
+  },
+  counterDisplay: {
+    display: 'inline',
+    fontSize: '2rem',
+    color: '#586272'
+  },
+  button: {
+    display: 'inline-block',
+    color: '#8f9eb7',
+    backgroundColor: '#586272',
+  }
+};
+
+export interface StateProps {
+  value: number;
+}
+
+export interface DispatchProps {
+  onIncrement: typeof pushIncrement;
+  onDecrement: typeof pushDecrement;
+}
+
+interface Props extends StateProps, DispatchProps {}
+
+interface State {}
+
+class Counter extends React.Component<Props, State> {
+  constructor(props: Props, state: State) {
+    super(props);
+  }
+
+  render() {
+    const { value, onIncrement, onDecrement } = this.props;
+    return (
+      <div className="counter" style={styles.counter}>
+        <div style={styles.counterDisplay}>{value}</div>
+        <button onClick={onIncrement} style={styles.button}>+</button>
+        <button onClick={onDecrement} style={styles.button}>-</button>
+      </div>
+    );
+  }
+}
+
+// const Counter = () => <div>Counter</div>;
 export default Counter;
