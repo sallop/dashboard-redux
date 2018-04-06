a1b718d4b38ca45e8e1ad2a865e7d1c2aa7b7b20
sallop <shiogai1987@yahoo.co.jp>
2018-04-06 20:29:55 +0900

    wip: restricted contents

--- src/containers/organisms/GlobalNavbar.tsx
+++ src/containers/organisms/GlobalNavbar.tsx
@@ -5,6 +5,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { Logo } from './logo.svg'; // you get React component
 import logo from './logo.svg';
 // import { default as Logo } from './logo.svg'; // svg url or base64 encoded data url
+import * as auth0 from 'auth0-js';
+import { loginRequest, logoutRequest } from '../../actions';
+
 import {
   Nav,
   Navbar,
@@ -21,10 +24,12 @@ interface Props {
   history: History;
   idToken?: string;
   // profile: Profile;
-  // actions: {
-  //   loginRequest: typeof loginRequest,
-  //   handleLogout: typeof handleLogout,
-  // };
+  profile: auth0.Auth0UserProfile;
+  actions: {
+    // handleLogin: typeof loginRequest,
+    handleLogin: typeof loginUser,
+    handleLogout: typeof logoutRequest,
+  };
 }
 
 interface State {
@@ -35,7 +40,7 @@ class GlobalNavbar extends React.Component<Props, State> {
 
   constructor(props: Props, state: State) {
     super(props);
-
+  
     this.login = this.login.bind(this);
     this.toggle = this.toggle.bind(this);
     this.state = {
@@ -73,6 +78,23 @@ class GlobalNavbar extends React.Component<Props, State> {
             }
             </Nav>
           </Collapse>
+          {
+            profile &&
+            <NavLink to="/books" activeStyle={activeStyle}>
+              <Label mx={3} style={{ cursor: 'pointer' }}>
+                Books
+              </Label>
+            </NavLink>
+          }
+          {
+            profile === undefined ?
+            <Button ml="auto" bg="green4" onClick={actions.handleLogin} style={{ cursor: 'pointer' }}>
+                Login
+            </Button> :
+              <Button  ml="auto" bg="red4" onClick={actions.handleLogout} style={{ cursor: 'pointer' }}>
+                Logout
+            </Button>
+          }
         </Navbar>
       </div>
     );
